64a596ec0fc1f8b288115ab4a38be85c513dd053
fjasensi <fjasensi@outlook.com>
2026-07-09 17:35:09 +0200

    Initial Commit

---
+++ tracking_hours/tracking_hoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  tracking_hoursApp.swift
+//  tracking_hours
+//
+//  Created by Francisco Javier Asensi Benito on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct tracking_hoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
